547cf7928f6e74f6b894e6d8974a85064355e821
Marko <marko.krtolica@conradconnect.de>
2020-04-04 13:58:56 +0200

    Added endpoint for creating and authenticating users

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from user import views
+
+# This is set to help identify which
+# app we create url from when I use reverse function
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+]
